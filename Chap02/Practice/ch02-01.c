06fc7ae1dd20f8a653791b9c8b05fa24fe5d28e7
iruma-tea <shusuke.think.pad@gmail.com>
2023-10-21 19:19:52 +0900

    最初のプログラムをコミット

---
+++ Chap02/Practice/ch02-01.c
@@ -0,0 +1,20 @@
+/*
+* 最初のプログラム
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1) {
+        puts("hello, world!!");
+    } else {
+        int sum = 0;
+        // インデックス0は無視する
+        for (int i = 1; i < argc; i++) {
+            sum += atoi(argv[i]);
+        }
+        printf("sum=%d\n", sum);
+    }
+    return 0;
+}
